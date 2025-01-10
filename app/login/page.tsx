82320c247dd7bdf7187e8a1a64509bed3ec49143
rajneeshrana0 <rajneeshrana038@gmail.com>
2025-01-10 08:06:41 +0530

    Enhance homepage with login button, update layout styles, and improve global CSS variables for better theming

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,7 @@ export default function Signin() {
                         </div>
                     </div>
                     <div className="pt-2">
-                        <LabelledInput label="Username" placeholder="harkirat@gmail.com" />
+                        <LabelledInput label="Username" placeholder="rajne@gmail.com" />
                         <LabelledInput label="Password" type={"password"} placeholder="123456" />
                         <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
