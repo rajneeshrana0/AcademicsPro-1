ae0f8a8789796b97da911b067a7a95aa368d94fa
rajneeshrana0 <rajneeshrana038@gmail.com>
2025-01-12 01:39:36 +0530

    Add placeholder content to Student Dashboard and comment out loading skeletons in Dashboard component

--- app/(dashboard)/student/page.tsx
+++ app/(dashboard)/student/page.tsx
@@ -3,6 +3,9 @@ export default function Page() {
     return (
         <>
         <h1>This is Our Student Dsahboard </h1>
+      <div>
+        asdffffffffffffffffffffffffffffffffffff
+      </div>
         </>
     )
 }
