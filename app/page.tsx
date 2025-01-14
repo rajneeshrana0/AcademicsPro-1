365e948b63cb5591a8d026c2d2309f558a2ccb8c
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2025-01-14 13:36:26 +0530

    Add Vercel Analytics integration to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 "use client"
 import LandingPage from '@/./app/components/landing/landingpage'
 import { SessionProvider } from 'next-auth/react';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
+
   return (
       <SessionProvider>
     <main className='overflow-x-hidden'>
 
-
+    <Analytics />
     <LandingPage />
   </main>
       </SessionProvider>
